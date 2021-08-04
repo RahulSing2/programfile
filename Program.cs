8e2e50fb4d707de6df5cdc3196d0339c2ba0b27a
RahulSing2 <88368239+RahulSing2@users.noreply.github.com>
2021-08-04 23:22:09 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace programfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
